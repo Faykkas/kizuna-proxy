3d1f231d566b7d9f9d32e7851f83d3410606a54d
Samuel Pechinez <samuel.pechinez@gmail.com>
2026-04-07 10:17:33 +0200

    add tawk.to live chat

--- app/layout.tsx
+++ app/layout.tsx
@@ -101,7 +101,25 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           ]
         })}} />
       </head>
-      <body>{children}<Analytics /></body>
+      <body>
+        {children}
+        <Analytics />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/69d4bd230846fc1c371afcfe/1jljg5kpl';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+              })();
+            `
+          }}
+        />
+      </body>
     </html>
   );
 }
